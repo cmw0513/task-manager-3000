abc69e9206a23b8ea984e22f0fc67b29241020fd
Xavier Defrang <xavier.defrang@gmail.com>
2015-09-28 10:39:44 +0200

    Add secure page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
+
+  skip_before_action :authenticate_account!, except: [:payment]
+
   def home
     @tasks        = Task.all
     @testimonials = Testimonial.random.limit(3)
+  end
+
+  def payment
   end
 end
