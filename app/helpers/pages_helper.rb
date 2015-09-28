5db6633553c54fd3d80b0f9a4048dda26347d2bd
Xavier Defrang <xavier.defrang@gmail.com>
2015-09-28 09:42:44 +0200

    Extract task view helpers

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,15 @@
 module PagesHelper
+
+  def task_done(task)
+    task.done? ? "[X]" : "[ ]"
+  end
+
+  def task_due_date(task)
+    "(due #{format_task_due_date(task)})" if task.due_date
+  end
+
+  def format_task_due_date(task)
+    task.due_date.strftime("%B %d")
+  end
+
 end
