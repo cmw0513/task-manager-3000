5513df4865388a161f4af9e4ac7a79b13d65b89a
Xavier Defrang <xavier.defrang@gmail.com>
2015-09-29 10:02:17 +0200

    Add bar

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,8 @@ class Account < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def bar
+    "bar" * 10
+  end
 end
